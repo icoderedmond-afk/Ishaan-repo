d536f1cf427368048299255fbd429a899ce5ea1e
icoderedmond-afk <icoderedmond@gmail.com>
2026-07-20 12:32:20 -0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,22 @@
+print("hello world")
+ answer = input("Whats your name? ")
+ print("Hello, " + answer)
+
+words = set()
+
+
+def check(word):
+ return word.lower() in words
+
+
+def load(dictionary):
+ with open(dictionary) as file:
+   words,update(fire.read().splitlines())
+   return True
+ 
+
+ def size():
+    return len(words)
+ 
+
+ def unload():
